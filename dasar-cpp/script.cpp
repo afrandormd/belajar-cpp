462e3001b6d4a231f331f655f1faeac2c2e9ee3c
afrandormd <afrandormd@gmail.com>
2023-02-22 21:53:46 +0700

    membuat struktur folder

---
+++ dasar-cpp/script.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string nama;
+    cout << "hello world";
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    cout << nama;
+    return 0;
+}
+
