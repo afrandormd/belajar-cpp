9a1127c2a8e9076f3e1d3e4571ef2f819b4d1bfe
afrandormd <afrandormd@gmail.com>
2023-02-23 21:07:11 +0700

    buat file baru latihan-rumus.cpp

---
+++ latihan-cpp/latihan-rumus.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // luas persegi
+    float sisi, luas;
+    cout << "Masukkan sisi: ";
+    cin >> sisi;
+    luas = sisi * sisi;
+    cout << "Luasnya adalah: " << luas;
+    return 0;
+}
