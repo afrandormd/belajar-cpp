86771f424846490374462a5a07ac3962677f4004
afrandormd <afrandormd@gmail.com>
2023-02-23 21:30:41 +0700

    menambahkan rumus persegi panjang

--- latihan-cpp/latihan-rumus.cpp
+++ latihan-cpp/latihan-rumus.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 int main(){
     // luas persegi
-    float sisi, luas;
+    float sisi, luas, panjang, lebar;
     cout << "Masukkan sisi: ";
     cin >> sisi;
     luas = sisi * sisi;
-    cout << "Luasnya adalah: " << luas;
-    return 0;
+    cout << "Luasnya persegi adalah: " << luas << "\n";
+
+    cout << "\n=====================\n";
+        // luas persegi panjang
+    cout << "\nMasukkan panjang: ";
+    cin >> panjang;
+    cout << "Masukkan Lebar: ";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luasnya persegi panjang adalah: " << luas;
+return 0;
 }
