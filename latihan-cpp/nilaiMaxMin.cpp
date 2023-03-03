7069b82c7de2b91e807f02c6bc0dd9bbab170dd4
afrandormd <afrandormd@gmail.com>
2023-03-03 10:30:29 +0700

    membuat file nilaiMaxMin.cpp

---
+++ latihan-cpp/nilaiMaxMin.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Program mencari nilai terbesar dan terkecil 
+    // create and initialitation variable
+    int nilai1, nilai2, nilai3, maxValue, minValue;
+    nilai1 = 0;
+    nilai2 = 0;
+    nilai3 = 0;
+
+    // input nilai 1
+    cout << "Masukkan nilai 1: ";
+    cin >> nilai1;
+    // input nilai 2
+    cout << "Masukkan nilai 2: ";
+    cin >> nilai2;
+    // input nilai 3
+    cout << "Masukkan nilai 3: ";
+    cin >> nilai3;
+    
+    // mencari nilai terbesar dan terkecil
+
+
+
+    return 0;
+}
